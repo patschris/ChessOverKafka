94a25175eb9172d532e8c96ae155b680a093d490
Christos Patsouras <32415524+patschris@users.noreply.github.com>
2019-04-11 13:02:06 +0300

    Add package resources.chess.images.board

--- src/main/java/chess/GameDisplay.java
+++ src/main/java/chess/GameDisplay.java
@@ -36,11 +36,10 @@ class GameDisplay extends Pad {
 
     /** Return an Image read from the resource named NAME. */
     private Image getImage(String name) {
-        InputStream in = getClass().getResourceAsStream("/chess/images/"
-            + name);
+        InputStream in = getClass().getResourceAsStream("/chess/images/" + name);
         try {
             return ImageIO.read(in);
-        } catch (IOException excp) {
+        } catch (IOException e) {
             return null;
         }
     }
@@ -61,21 +60,20 @@ class GameDisplay extends Pad {
     public synchronized void paintComponent(Graphics2D g) {
         Rectangle b = g.getClipBounds();
         g.fillRect(0, 0, b.width, b.height);
-        g.drawImage(getImage("chessboard.jpg"), 0, 0, BOARD, BOARD, null);
+        g.drawImage(getImage("board/chessboard.jpg"), 0, 0, BOARD, BOARD, null);
         if (_game.inCheck(_game.turn())) {
-            g.drawImage(getImage("inCheck.png"),
+            g.drawImage(getImage("board/inCheck.png"),
                 CELL * _game.kingX(_game.turn()) + MARGIN,
                 CELL * _game.kingY(_game.turn()) + MARGIN, CELL, CELL, null);
         }
         if (_game.selectedX() != -1) {
-            g.drawImage(getImage("selected.png"),
+            g.drawImage(getImage("board/selected.png"),
                 CELL * _game.selectedX() + MARGIN,
                 CELL * _game.selectedY() + MARGIN, CELL, CELL, null);
         }
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                paintPiece(g, _game.get(i, j),
-                    CELL * i + MARGIN, CELL * j + MARGIN);
+                paintPiece(g, _game.get(i, j), CELL * i + MARGIN, CELL * j + MARGIN);
             }
         }
     }
