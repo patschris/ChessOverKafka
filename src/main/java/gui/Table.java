ca1ee3cc02a27853a1f5d8202c37095bfa52fd73
Christos Patsouras <chpatsouras@gmail.com>
2019-04-27 19:35:34 +0300

    Add Kafka logic to table gui

--- src/main/java/gui/Table.java
+++ src/main/java/gui/Table.java
@@ -4,10 +4,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+
+import chess.game.Game;
+import chess.game.GameCore;
+import chess.pieces.PieceColor;
+import kafka_consumer_producer.ConsumerCreator;
+import kafka_consumer_producer.ProducerCreator;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import javax.swing.*;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,249 +33,324 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Table extends JFrame {
 
-    private String baseUrl;
-    private String whoAmI;
-    private JLabel title;
-    private JLabel subtitle;
-    private JLabel gifLabel;
-    private JComboBox<String> dropdown;
-    private DefaultListModel model;
-    private JList list;
-    private JScrollPane scrollPane;
-    private JButton clearButton;
-    private JButton submitButton;
-    private JButton refreshButton;
+	private String baseUrl;
+	private String whoAmI;
+	private JLabel title;
+	private JLabel subtitle;
+	private JLabel gifLabel;
+	private JComboBox<String> dropdown;
+	private DefaultListModel model;
+	private JList list;
+	private JScrollPane scrollPane;
+	private JButton clearButton;
+	private JButton submitButton;
+	private JButton refreshButton;
 
-    public Table(String userLoggedIn) {
-        super("Table");
-        whoAmI = userLoggedIn;
-        setSize(700,300);	// size of login window
-        setLayout(null);	// no default layout is used
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// close operation
-        addTitle();
-        createDropDown();
+	public Table(String userLoggedIn) {
+		super("Table");
+		whoAmI = userLoggedIn;
+		setSize(700,300);	// size of login window
+		setLayout(null);	// no default layout is used
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// close operation
+		addTitle();
+		createDropDown();
 
-        createSubtitle();
-        addImageLabel();
-        addList();
-        addButtons();
+		createSubtitle();
+		addImageLabel();
+		addList();
+		addButtons();
 
-        getBaseUrl();
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent){
-                removeTable();
-                System.exit(0);
-            }
-        });
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
-        setResizable(false);
-        setVisible(true);
-    }
+		getBaseUrl();
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent){
+				removeTable();
+				System.exit(0);
+			}
+		});
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
+		setResizable(false);
+		setVisible(true);
+	}
 
-    /**
-     * 	Creating a label to inform the user.
-     */
-    private void addTitle(){
-        title = new JLabel("Create a new table or join another table?");
-        title.setSize(300, 50);
-        title.setLocation(100, 15);
-        add(title);
-    }
+	/**
+	 * 	Creating a label to inform the user.
+	 */
+	private void addTitle(){
+		title = new JLabel("Create a new table or join another table?");
+		title.setSize(300, 50);
+		title.setLocation(100, 15);
+		add(title);
+	}
 
-    private void createDropDown(){
-        String[] selections = new String[] {"<html><b><i>Select Option</i></b></html>", "Create table", "Join table"};
-        dropdown = new JComboBox<>(selections);
-        dropdown.setSize(150, 25);
-        dropdown.setLocation(400, 25);
-        dropdown.setSelectedIndex(0);
-        dropdown.addActionListener(new DropDownListener());
-        add(dropdown);
-    }
+	private void createDropDown(){
+		String[] selections = new String[] {"<html><b><i>Select Option</i></b></html>", "Create table", "Join table"};
+		dropdown = new JComboBox<>(selections);
+		dropdown.setSize(150, 25);
+		dropdown.setLocation(400, 25);
+		dropdown.setSelectedIndex(0);
+		dropdown.addActionListener(new DropDownListener());
+		add(dropdown);
+	}
 
-    private void createSubtitle (){
-        subtitle=new JLabel();
-        subtitle.setSize(300, 30);
-        subtitle.setLocation(270, 35);
-        add(subtitle);
-    }
+	private void createSubtitle (){
+		subtitle=new JLabel();
+		subtitle.setSize(300, 30);
+		subtitle.setLocation(270, 35);
+		add(subtitle);
+	}
 
-    private void addImageLabel() {
-        URL url = this.getClass().getResource("/chess/images/gui/loading.gif");
-        Icon loadingGif = new ImageIcon(url);
-        gifLabel = new JLabel(loadingGif);
-        gifLabel.setSize(150, 80);
-        gifLabel.setLocation(270, 100);
-        add(gifLabel);
-        gifLabel.setVisible(false);
-    }
+	private void addImageLabel() {
+		URL url = this.getClass().getResource("/chess/images/gui/loading.gif");
+		Icon loadingGif = new ImageIcon(url);
+		gifLabel = new JLabel(loadingGif);
+		gifLabel.setSize(150, 80);
+		gifLabel.setLocation(270, 100);
+		add(gifLabel);
+		gifLabel.setVisible(false);
+	}
 
-    private void addList(){
-        model = new DefaultListModel();
+	private void addList(){
+		model = new DefaultListModel();
 
-        list = new JList(model);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list = new JList(model);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-        scrollPane = new JScrollPane(list);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(100, 75, 450, 130);
-        add(scrollPane);
-        scrollPane.setVisible(false);
-    }
+		scrollPane = new JScrollPane(list);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setBounds(100, 75, 450, 130);
+		add(scrollPane);
+		scrollPane.setVisible(false);
+	}
 
-    private void addButtons(){
-        clearButton = new JButton("Clear");		//	creates Clear button,
-        clearButton.setSize(100, 30);		//	sets its size and location
-        clearButton.setLocation(220, 225);
-        clearButton.setActionCommand("Clear");	// sets action command for Cancel button
-        clearButton.addActionListener(new ButtonPressedListener(this));	// sets listener for Cancel button
-        submitButton = new JButton("Play");		// 	creates Login button
-        submitButton.setSize(100, 30);
-        submitButton.setLocation(350, 225);
-        submitButton.setActionCommand("Play");	// sets action command for Sumbit button
-        submitButton.addActionListener(new ButtonPressedListener(this));	// sets listener for Submit button
-        URL url = this.getClass().getResource("/chess/images/gui/refresh.png");
-        ImageIcon icon = new ImageIcon(url);
-        refreshButton = new JButton(icon);
+	private void addButtons(){
+		clearButton = new JButton("Clear");		//	creates Clear button,
+		clearButton.setSize(100, 30);		//	sets its size and location
+		clearButton.setLocation(220, 225);
+		clearButton.setActionCommand("Clear");	// sets action command for Cancel button
+		clearButton.addActionListener(new ButtonPressedListener(this));	// sets listener for Cancel button
+		submitButton = new JButton("Play");		// 	creates Login button
+		submitButton.setSize(100, 30);
+		submitButton.setLocation(350, 225);
+		submitButton.setActionCommand("Play");	// sets action command for Sumbit button
+		submitButton.addActionListener(new ButtonPressedListener(this));	// sets listener for Submit button
+		URL url = this.getClass().getResource("/chess/images/gui/refresh.png");
+		ImageIcon icon = new ImageIcon(url);
+		refreshButton = new JButton(icon);
 
-        refreshButton.setSize(50,50);
-        refreshButton.setLocation(560, 120);
-        refreshButton.setActionCommand("Refresh");
-        refreshButton.addActionListener(new ButtonPressedListener(this));
-        add(clearButton);
-        add(submitButton);
-        add(refreshButton);
-        clearButton.setVisible(false);
-        submitButton.setVisible(false);
-        refreshButton.setVisible(false);
-    }
+		refreshButton.setSize(50,50);
+		refreshButton.setLocation(560, 120);
+		refreshButton.setActionCommand("Refresh");
+		refreshButton.addActionListener(new ButtonPressedListener(this));
+		add(clearButton);
+		add(submitButton);
+		add(refreshButton);
+		clearButton.setVisible(false);
+		submitButton.setVisible(false);
+		refreshButton.setVisible(false);
+	}
 
-    private void getBaseUrl () {
-        try (FileInputStream fileInput = new FileInputStream( new File("src/main/resources/chess/configurations/config.properties"))) {
-            Properties properties = new Properties();
-            properties.load(fileInput);
-            baseUrl = properties.getProperty("restAddress");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	private void getBaseUrl () {
+		try (FileInputStream fileInput = new FileInputStream( new File("src/main/resources/chess/configurations/config.properties"))) {
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			baseUrl = properties.getProperty("restAddress");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void removeTable () {
-        Client.create().resource(baseUrl + "/removetable/" + whoAmI).get(ClientResponse.class);
-    }
+	private void removeTable () {
+		Client.create().resource(baseUrl + "/removetable/" + whoAmI).get(ClientResponse.class);
+	}
 
-    private void addTable () {
-        Client.create().resource(baseUrl + "/newtable/" + whoAmI).get(ClientResponse.class);
-    }
+	private void addTable () {
+		Client.create().resource(baseUrl + "/newtable/" + whoAmI).get(ClientResponse.class);
+	}
 
-    private void getOpponents () {
-        ClientResponse response = Client.create().resource(baseUrl + "/getopponents/" + whoAmI).get(ClientResponse.class);
-        JsonArray players = new JsonParser().parse(response.getEntity(String.class)).getAsJsonArray();
-        model.clear();
-        for (JsonElement player:players) {
-            model.addElement(player.getAsJsonObject().get("name").getAsString());
-        }
-    }
+	private void addTopics () {
+		Client.create().resource(baseUrl + "/createtopics/" + whoAmI).get(ClientResponse.class);
+	}
 
-
-    private class DropDownListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            JComboBox<String> combo = (JComboBox<String>) actionEvent.getSource();
-            String selectedOption = (String) combo.getSelectedItem();
-
-           if (selectedOption.equals("Create table")) {
-        	   anOptionSelected();
-                onCreateTableSelected();
-            }
-            else if (selectedOption.equals("Join table")) {
-            	anOptionSelected();
-            	onJoinTableSelected();
-            }
-        }
-
-        private void anOptionSelected() {
-        	dropdown.setVisible(false);
-            title.setVisible(false);
-        }
-        
-        private void onCreateTableSelected(){
-            subtitle.setText("Wait for an opponent");
-            gifLabel.setVisible(true);
-            new SwingWorker<Void, Void>() {
-                protected Void doInBackground() {
-                    addTable();
-
-                    //eimai o white
-                    // anoikse kafka topic
+	private void getOpponents () {
+		ClientResponse response = Client.create().resource(baseUrl + "/getopponents/" + whoAmI).get(ClientResponse.class);
+		JsonArray players = new JsonParser().parse(response.getEntity(String.class)).getAsJsonArray();
+		model.clear();
+		for (JsonElement player:players) {
+			model.addElement(player.getAsJsonObject().get("name").getAsString());
+		}
+	}
 
 
-                    return null;
-                }
+	private class DropDownListener implements ActionListener {
 
-                @Override
-                protected void done() {
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+			JComboBox<String> combo = (JComboBox<String>) actionEvent.getSource();
+			String selectedOption = (String) combo.getSelectedItem();
 
-                }
-            }.execute();
-        }
+			if (selectedOption.equals("Create table")) {
+				anOptionSelected();
+				onCreateTableSelected();
+			}
+			else if (selectedOption.equals("Join table")) {
+				anOptionSelected();
+				onJoinTableSelected();
+			}
+		}
 
-        private void onJoinTableSelected(){
-            subtitle.setText("Select an opponent");
-            scrollPane.setVisible(true);
-            clearButton.setVisible(true);
-            submitButton.setVisible(true);
-            refreshButton.setVisible(true);
-            new SwingWorker<Void, Void>() {
-                protected Void doInBackground()  {
-                    getOpponents();
-                    return null;
-                }
+		private void anOptionSelected() {
+			dropdown.setVisible(false);
+			title.setVisible(false);
+		}
 
-                @Override
-                protected void done() {
+		private void onCreateTableSelected(){
+			subtitle.setText("Wait for an opponent");
+			gifLabel.setVisible(true);
+			new SwingWorker<Void, Void>() {
+				protected Void doInBackground() throws InterruptedException {
+					addTable();
 
-                }
-            }.execute();
-        }
-    }
+					addTopics();
 
-    private class ButtonPressedListener implements ActionListener {
 
-        private Table window;
+					//create the white_consumer and wait for someone to join you
+					Consumer<Long, String> white_consumer = ConsumerCreator.createConsumer(whoAmI);
 
-        ButtonPressedListener (Table table) {
-            this.window = table;
-        }
+					String msg = "";
 
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            if (actionEvent.getActionCommand().equals("Play")) {
-                String name = (String) list.getSelectedValue();
-                if (name==null) {
-                    JOptionPane.showMessageDialog(this.window, "No opponent selected");
-                }
-                else {
-                    ObjectNode objectNode = new ObjectMapper().createObjectNode();
-                    objectNode.put("username", name);
-                    System.out.println(objectNode.toString());
+					System.out.println("Waiting For Message!");
 
-                    // perimene apantisi gia syndesi kai arxise neo paixnidi
+					while (true) {
+						ConsumerRecords<Long, String> consumerRecords = white_consumer.poll(10);
+						if (consumerRecords.count() == 0) {
+							continue;
+						}
+						for(ConsumerRecord<Long, String> record: consumerRecords) {
+							msg = (String) record.value();
+							System.out.println(msg);
+							//JOptionPane.showMessageDialog(null, record.value());	
+						}
+						// commits the offset of record to broker.
+						white_consumer.commitAsync();
 
-                }
-            }
-            else if (actionEvent.getActionCommand().equals("Refresh")) {
-                getOpponents();
-            }
-            else {
-                list.clearSelection();
-            }
-        }
-    }
+						break;
+					}
+
+					white_consumer.close();
+
+					String opponent = msg;
+
+					System.out.println("Iam: " + whoAmI);
+					System.out.println("Opponent: " + opponent);
+
+					System.out.println("Ready to play!");
+
+					dispose();
+
+
+					Game g = new Game(PieceColor.WHITE);
+					GameCore gamec = new GameCore(PieceColor.WHITE, g, whoAmI, opponent);
+					gamec.startgame(whoAmI, opponent);
+
+					return null;
+				}
+
+				@Override
+				protected void done() {
+
+				}
+			}.execute();
+		}
+
+		private void onJoinTableSelected(){
+			subtitle.setText("Select an opponent");
+			scrollPane.setVisible(true);
+			clearButton.setVisible(true);
+			submitButton.setVisible(true);
+			refreshButton.setVisible(true);
+			new SwingWorker<Void, Void>() {
+				protected Void doInBackground()  {
+					getOpponents();
+					return null;
+				}
+
+				@Override
+				protected void done() {
+
+				}
+			}.execute();
+		}
+	}
+
+	private class ButtonPressedListener implements ActionListener {
+
+		private Table window;
+
+		ButtonPressedListener (Table table) {
+			this.window = table;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+			if (actionEvent.getActionCommand().equals("Play")) {
+				String opponent = (String) list.getSelectedValue();
+				if (opponent == null) {
+					JOptionPane.showMessageDialog(this.window, "No opponent selected");
+				}
+				else {
+					addTopics();
+
+					//send your name to your opponent
+
+					Producer<Long, String> black_producer = ProducerCreator.createProducer();
+
+					@SuppressWarnings("unchecked")
+					ProducerRecord<Long, String> record = new ProducerRecord(opponent , whoAmI);
+
+					black_producer.send(record);
+
+					black_producer.close();
+
+					System.out.println("Iam: " + whoAmI);
+					System.out.println("Opponent: " + opponent);
+
+
+					//begin the game
+					System.out.println("Ready to play!");
+
+					dispose();
+
+					new SwingWorker<Void, Void>() {
+
+						@Override
+						protected Void doInBackground() throws Exception {
+							Game g = new Game(PieceColor.BLACK);
+							GameCore gamec = new GameCore(PieceColor.BLACK, g, opponent, whoAmI);
+							gamec.startgame(opponent , whoAmI);
+							return null;
+						}
+
+						@Override
+						protected void done() {
+						}
+					}.execute();
+
+
+				}
+			}
+			else if (actionEvent.getActionCommand().equals("Refresh")) {
+				getOpponents();
+			}
+			else {
+				list.clearSelection();
+			}
+		}
+	}
 }
